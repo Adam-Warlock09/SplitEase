924990918cee573cc649cf93678ef440042bb78c
Adam-Warlock09 <aricm1809@gmail.com>
2025-05-27 14:02:54 +0530

    Added check to load .env only when locally run

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,25 +1,26 @@
 package main
 
 import (
-
 	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/Adam-Warlock09/SplitEase/backend/internal/config"
 	"github.com/Adam-Warlock09/SplitEase/backend/internal/middleware"
 	"github.com/Adam-Warlock09/SplitEase/backend/internal/router"
 
 	"github.com/joho/godotenv"
-
 )
 
 func main() {
 
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
+	if os.Getenv("RENDER") == "" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
 	}
 
 	config.ConnectDB()
@@ -35,7 +36,7 @@ func main() {
 
 	fmt.Println("Server starting on http://localhost" + configurer.Port)
 	
-	err = http.ListenAndServe(configurer.Port, handler)
+	err := http.ListenAndServe(configurer.Port, handler)
 
 	if err != nil {
 		log.Fatal("Server failed : ", err)
