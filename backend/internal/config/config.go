83111804a632231726d57ae8178369c2bd11d684
Adam-Warlock09 <aricm1809@gmail.com>
2025-05-27 14:36:12 +0530

    Bug fixes for port handling in render for backend hosting

--- backend/internal/config/config.go
+++ backend/internal/config/config.go
@@ -7,6 +7,12 @@ type ServerConfig struct {
 }
 
 func LoadConfig() *ServerConfig {
+
+	port := os.Getenv("PORT")
+	if len(port) > 0 && port[0] != ':' {
+		port = ":" + port
+	}
+	
 	return &ServerConfig{
 		Port: os.Getenv("PORT"),
 	}
